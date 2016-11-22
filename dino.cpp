9745834361df5338a49b256747dfc90d11c12f20
Mikhail Abramov <beavisabra@yandex.ru>
2016-11-22 14:51:31 +0400

    Example of main file (creating of dino.cpp)

---
+++ dino.cpp
@@ -0,0 +1,15 @@
+#include TXLib.h
+#include dinosaur.cpp
+
+int main ()
+{
+    int x = 50, y = 100;
+    double vx = 5.0, vy = 3.0;
+    
+    txCreateWindow(600, 400);
+    while(!GetAsyncKeyState(VK_ESCAPE)) {
+        moveDino(x, y, *vx, *vy);
+    }
+    
+    return 0;
+}
